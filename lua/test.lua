b2dc599734b5c8844c5671da5ae2e2ede952cc1e
nuclear.fake <nuclearfake9@gmail.com>
2019-10-23 15:00:26 +0800

    update

--- lua/test.lua
+++ lua/test.lua
@@ -15,14 +15,6 @@ qtiny.NanoLocalRegister({
     end
 })
 
-qtiny.NanoLocalRegister({
-    Address = "qam.lua.here",
-    Handler = function(msg)
-        local info = debug.getinfo(1,'S');
-        qtiny.MessageReply(msg, 0, "here is the money " .. nodeId .. " " .. info.source)
-    end
-})
-
 qtiny.AddCloseHandler(function ()
     tina.GetLogger().Println("i am close!")
 end)
--- lua/test2.lua
+++ lua/test2.lua
@@ -3,6 +3,19 @@ local nodeId = tina.GetNodeId()
 qtiny.NanoLocalRegister({
     Address = "qam.lua.test2",
     Handler = function(msg)
+        tina.GetLogger().Println("test!")
         qtiny.MessageReply(msg, 0, "test " .. nodeId)
     end
 })
+
+qtiny.NanoLocalRegister({
+    Address = "qam.lua.err2",
+    Handler = function(msg)
+        qtiny.MessageError(msg, 0, "testing error " .. nodeId)
+    end
+})
+
+qtiny.AddCloseHandler(function()
+    tina.GetLogger().Println("i am close!")
+end)
+
