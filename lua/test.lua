71a055a64cb10ffeff069cf562b12c98d6eb0533
nuclear.fake <nuclearfake9@gmail.com>
2019-10-28 11:55:56 +0800

    update

--- lua/test.lua
+++ lua/test.lua
@@ -7,18 +7,18 @@ qtiny.NanoLocalRegister({
     Handler = function(msg)
         tina.GetLogger().Println("test!")
 
-        qtiny.MsgEasy(msg)
+        qmsg.Easy(msg)
 
         local data = json.decode(theM.Data)
 
-        qtiny.MsgReply(msg, 0, data["hello"] .. " " .. nodeId)
+        qmsg.Reply(msg, 0, data["hello"] .. " " .. nodeId)
     end
 })
 
 qtiny.NanoLocalRegister({
     Address = "qam.lua.err",
     Handler = function(msg)
-        qtiny.MsgError(msg, 0, "testing error " .. nodeId)
+        qmsg.Error(msg, 0, "testing error " .. nodeId)
     end
 })
 
--- lua/test2.lua
+++ lua/test2.lua
@@ -4,14 +4,14 @@ qtiny.NanoLocalRegister({
     Address = "qam.lua.test2",
     Handler = function(msg)
         tina.GetLogger().Println("test!")
-        qtiny.MsgReply(msg, 0, "test " .. nodeId)
+        qmsg.Reply(msg, 0, "test " .. nodeId)
     end
 })
 
 qtiny.NanoLocalRegister({
     Address = "qam.lua.err2",
     Handler = function(msg)
-        qtiny.MsgError(msg, 0, "testing error " .. nodeId)
+        qmsg.Error(msg, 0, "testing error " .. nodeId)
     end
 })
 
